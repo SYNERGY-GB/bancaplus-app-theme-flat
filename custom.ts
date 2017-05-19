395d7f578d1eb8854cb31f6626e46ae3bf41eb97
emmanueldeaguiar <emmanuel.deaguiar@gmail.com>
2017-05-19 13:44:34 -0400

    Added custom.ts

---
+++ custom.ts
@@ -0,0 +1,22 @@
+export var customize = (metadata : Megazord.ContainerScreenList) : void => {
+  //Default customization of metadata (does nothing).
+  metadata['dashboard.dashboard']['params']['modalAnimation'] = 'scale-out';
+
+  //templates
+    //flat
+    metadata['dashboard.dashboard']['template'] = 'dashboard.html';
+    metadata['appMenu.summary']['template'] = 'financial-summary.html';
+    metadata['appMenu.instrumentDetail']['template'] = 'instrument-detail.html';
+    metadata['dashboard.login']['template'] = 'login.html';
+    metadata['appMenu.p2pTransfer']['template'] = 'p2p-transfer.html';
+    metadata['appMenu.transferAmount']['template'] = 'transfer-amount.html';
+    metadata['appMenu.transferConfirm']['template'] = 'transfer-confirm.html';
+    metadata['appMenu.transferResult']['template'] = 'transfer-result.html';
+    metadata['appMenu.transferSelection']['template'] = 'transfer-selection.html';
+    metadata['appMenu.transfer']['template'] = 'transfer.html';
+
+  //customize.json
+  
+}
+
+//End of custom.ts
